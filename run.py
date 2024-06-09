6c5d211bf97d13acc695a7201c5f59c2de5f782a
Abbaas Zubair <abbaasibnzubair@gmail.com>
2024-06-09 18:33:48 +0000

    Update run.py to calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -82,7 +82,7 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated successfully.\n")
 
 
-#def calculate_surplus_data(sales_row):
+def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
 
@@ -127,7 +127,21 @@ def get_last_5_entries_sales():
 
     return columns
 
-sales_columns = get_last_5_entries_sales()
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%  
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column)  / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 def main():
     """
     Run all program functions
@@ -138,7 +152,10 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
     update_worksheet(new_surplus_data, "surplus")
-    get_last_5_entries_sales()
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches Data Automation")
-#main()
+main()
+
